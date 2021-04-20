1df853352277361a8e8d6ec53a3c21bdc358e522
Joe Manthey <jdmanthey@gmail.com>
2021-04-20 16:56:26 -0500

    Create 01_prep_reference_genome.sh

---
+++ 01_prep_reference_genome.sh
@@ -0,0 +1,9 @@
+cd references
+
+samtools faidx FicAlb1.5__GCA_000247815.2.fna
+
+bwa index FicAlb1.5__GCA_000247815.2.fna
+
+java -jar picard.jar CreateSequenceDictionary R=/home/jmanthey/references/FicAlb1.5__GCA_000247815.2.fna \
+O=/home/jmanthey/references/FicAlb1.5__GCA_000247815.2.dict
+
