111b42249277b002b009c47deaef5418c8323207
Joe Manthey <jdmanthey@gmail.com>
2024-09-09 12:01:46 -0500

    Update 01_prep_reference_genome.sh

--- 01_prep_reference_genome.sh
+++ 01_prep_reference_genome.sh
@@ -1,9 +1,13 @@
+interactive -p nocona
+
 cd references
+
+source activate bcftools
+
+bwa-mem2 index FicAlb1.5__GCA_000247815.2.fna
 
 samtools faidx FicAlb1.5__GCA_000247815.2.fna
 
-bwa index FicAlb1.5__GCA_000247815.2.fna
-
-java -jar picard.jar CreateSequenceDictionary R=/home/jmanthey/references/FicAlb1.5__GCA_000247815.2.fna \
+java -jar picard.jar CreateSequenceDictionary \
+R=/home/jmanthey/references/FicAlb1.5__GCA_000247815.2.fna \
 O=/home/jmanthey/references/FicAlb1.5__GCA_000247815.2.dict
-
